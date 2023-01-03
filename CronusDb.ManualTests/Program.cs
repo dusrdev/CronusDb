81e75a6dfff3ff8aa051e55023b8868ac308e85c
David Shnayder <dusrdev@outlook.com>
2023-01-03 20:03:29 +0200

    Added manual tests for things where unit testing fails

---
+++ CronusDb.ManualTests/Program.cs
@@ -0,0 +1,18 @@
+﻿using CronusDb;
+
+var config = new CronusDbConfiguration<int>() {
+    Path = @".\encrypted.db",
+    EncryptionKey = "1q2w3e4r5t",
+    Serializer = static x => x.ToString(),
+    Deserializer = static x => int.Parse(x)
+};
+
+//var db = await CronusDb<int>.Create(config);
+
+//db.Upsert("David", 25);
+
+//await db.Serialize();
+
+//var rdb = await CronusDb<int>.Create(config);
+
+//Console.WriteLine($"David => {rdb.Get("David")}");
