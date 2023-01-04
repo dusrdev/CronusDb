aab0e83d47328da0c400d207a0b40f88115511cc
David Shnayder <dusrdev@outlook.com>
2023-01-04 12:18:33 +0200

    Create base database type with default implementations

---
+++ CronusDb/CronusDatabase.cs
@@ -0,0 +1,43 @@
+﻿namespace CronusDb;
+
+public abstract class CronusDatabase<T> {
+    /// <summary>
+    /// Triggered when an item is upserted.
+    /// </summary>
+    /// <remarks>
+    /// You can use this to hook cache invalidation with <see cref="RemoveAny(Func{T, bool})"/>.
+    /// </remarks>
+    public event EventHandler? ItemUpserted;
+
+    internal virtual void OnItemUpserted(EventArgs e) {
+        ItemUpserted?.Invoke(this, e);
+    }
+
+    /// <summary>
+    /// Triggered when an item is removed.
+    /// </summary>
+    /// <remarks>
+    /// You can use this to hook cache invalidation with <see cref="RemoveAny(Func{T, bool})"/>.
+    /// </remarks>
+    public event EventHandler? ItemRemoved;
+
+    internal virtual void OnItemRemoved(EventArgs e) {
+        ItemRemoved?.Invoke(this, e);
+    }
+
+    public abstract bool ContainsKey(string key);
+
+    public abstract T? Get(string key);
+
+    public abstract void Upsert(string key, T value);
+
+    public abstract bool Remove(string key);
+
+    public abstract void RemoveAny(Func<T, bool> selector);
+
+    public abstract Task Serialize();
+
+    internal abstract Task SerializeWithEncryption(Dictionary<string, string> data);
+
+    internal abstract Task SerializeWithoutEncryption(Dictionary<string, string> data);
+}
