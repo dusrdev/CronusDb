15c128d08b724719be949ad2615ac5fcb96df24b
David Shnayder <dusrdev@outlook.com>
2023-01-04 12:20:14 +0200

    Added serializable configuration file

---
+++ CronusDb/SerializableDatabaseConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace CronusDb;
+
+public record SerializableDatabaseConfiguration<T> {
+    /// <summary>
+    /// The path to the database file, including extension.
+    /// </summary>
+    public required string Path { get; init; }
+
+    /// <summary>
+    /// Encryption key for encrypting the database file with AES256.
+    /// </summary>
+    /// <remarks>
+    /// <para>Leave as null if encryption is not required.</para>
+    /// <para>Only effects serialization and deserialization</para>
+    /// </remarks>
+    public string? EncryptionKey { get; init; }
+
+    /// <summary>
+    /// Serialization function for <typeparamref name="T"/> into string.
+    /// </summary>
+    public required Func<T, string> ToStringConverter { get; init; }
+
+    /// <summary>
+    /// Deserialization function for string into <typeparamref name="T"/>.
+    /// </summary>
+    public required Func<string, T> FromStringConverter { get; init; }
+}
