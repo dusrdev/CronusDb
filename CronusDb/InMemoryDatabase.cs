5f6f858c574a1a75b4dcf3b7d3620ef85186b25f
David Shnayder <dusrdev@outlook.com>
2023-01-04 12:18:59 +0200

    Created In-Memory only database for best performance

---
+++ CronusDb/InMemoryDatabase.cs
@@ -0,0 +1,75 @@
+﻿namespace CronusDb;
+
+public sealed class InMemoryDatabase<T> : CronusDatabase<T> {
+    private readonly Dictionary<string, T> _data;
+
+    // This constructor is used for an In-Memory only instance
+    internal InMemoryDatabase(Dictionary<string, T> data) {
+        _data = data;
+    }
+
+    /// <summary>
+    /// Returns the value if it exists, otherwise the default for <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="key"></param>
+    public override T? Get(string key) => _data.TryGetValue(key, out var value) ? value : default;
+
+    /// <summary>
+    /// Inserts or updates
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public override void Upsert(string key, T value) {
+        _data[key] = value;
+        OnItemUpserted(EventArgs.Empty);
+    }
+
+    /// <summary>
+    /// Checks whether the database contains the <paramref name="key"/>.
+    /// </summary>
+    /// <param name="key"></param>
+    public override bool ContainsKey(string key) => _data.ContainsKey(key);
+
+    /// <summary>
+    /// Removes a value by <paramref name="key"/>
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if successful, false if database did not contain <paramref name="key"/>.</returns>
+    public override bool Remove(string key) {
+        var output = _data.Remove(key);
+        if (output) {
+            OnItemRemoved(EventArgs.Empty);
+        }
+        return output;
+    }
+
+    /// <summary>
+    /// Loops through all database entries and removes any for which the <paramref name="selector"/> returns true.
+    /// </summary>
+    /// <param name="selector"></param>
+    /// <remarks>
+    /// This is the main way to perform cache invalidation.
+    /// </remarks>
+    public override void RemoveAny(Func<T, bool> selector) {
+        foreach (var (k, v) in _data) {
+            if (selector.Invoke(v)) {
+                Remove(k);
+            }
+        }
+    }
+
+    /// <summary>
+    /// As this is an In-Memory only database, it does nothing.
+    /// </summary>
+    public override Task Serialize() {
+        return Task.CompletedTask;
+    }
+
+    internal override Task SerializeWithEncryption(Dictionary<string, string> data) {
+        throw new InvalidOperationException("Cannot serialize an In-Memory only database.");
+    }
+
+    internal override Task SerializeWithoutEncryption(Dictionary<string, string> data) {
+        throw new InvalidOperationException("Cannot serialize an In-Memory only database.");
+    }
+}
