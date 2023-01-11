bbb0289c02396fcf3d5e1e8ebd3a653cc8bdaa2c
David Shnayder <dusrdev@outlook.com>
2023-01-11 18:37:04 +0200

    Added extensions to make aes easier to implement

---
+++ CronusDb/Extensions.cs
@@ -0,0 +1,13 @@
+﻿namespace CronusDb;
+
+internal static class Extensions {
+    internal static string Encrypt(this string value, string key) {
+        using var aes = new CronusAesProvider(key);
+        return aes.Encrypt(value);
+    }
+
+    internal static string Decrypt(this string value, string key) {
+        using var aes = new CronusAesProvider(key);
+        return aes.Decrypt(value);
+    }
+}
