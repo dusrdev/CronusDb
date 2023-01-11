01aec006d4c107b00c676b39864e24ee46b6fca7
David Shnayder <dusrdev@outlook.com>
2023-01-11 18:38:03 +0200

    Added config for new type of database

---
+++ CronusDb/PolymorphicConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace CronusDb;
+
+/// <summary>
+/// Configuration for the polymorphic database
+/// </summary>
+public record PolymorphicConfiguration {
+    /// <summary>
+    /// The path to which the database file will be saved.
+    /// </summary>
+    public required string Path { get; init; }
+
+    /// <summary>
+    /// General encryption key, the entire file will be encrypted with this.
+    /// </summary>
+    public string? EncryptionKey { get; init; }
+}
