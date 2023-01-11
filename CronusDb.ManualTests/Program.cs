2c815072de952f1325950e99ee576b1bf71ca6c6
David Shnayder <dusrdev@outlook.com>
2023-01-11 18:39:32 +0200

    Added tests that are bugged in unit

--- CronusDb.ManualTests/Program.cs
+++ CronusDb.ManualTests/Program.cs
@@ -2,6 +2,25 @@
 
 using System.Diagnostics;
 
+string[] originals = {
+            "David",
+            """{["ben", "alex"]}""",
+            "dasdasd\"sadad"
+        };
+
+var aes = new CronusAesProvider("1q2w3e4r5t");
+
+var processed = originals.Select(aes.Encrypt).ToArray();
+var decrypted = processed.Select(aes.Decrypt).ToArray();
+
+for (int i = 0; i < originals.Length; i++) {
+    Console.WriteLine($"Original: {originals[i]}");
+    Console.WriteLine($"Processed: {processed[i]}");
+    Console.WriteLine($"Decrypted: {decrypted[i]}");
+    Console.WriteLine("");
+    Debug.Assert(originals[i] == decrypted[i]);
+}
+
 //var config = new CronusDbConfiguration<int>() {
 //    Path = @".\encrypted.db",
 //    EncryptionKey = "1q2w3e4r5t",
