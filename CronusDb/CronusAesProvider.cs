114589f43b474d6466e1c21b9f7ad40e1dc0ab64
David Shnayder <dusrdev@outlook.com>
2023-01-11 18:36:42 +0200

    Upgraded stability and increased efficiency

--- CronusDb/CronusAesProvider.cs
+++ CronusDb/CronusAesProvider.cs
@@ -4,35 +4,48 @@ using System.Text;
 namespace CronusDb;
 
 internal sealed class CronusAesProvider : IDisposable {
-    private readonly byte[] _vector = { 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 23, 19, 17 };
-    private readonly ICryptoTransform? _encryptor;
-    private readonly ICryptoTransform? _decryptor;
+    private static readonly byte[] _vector = { 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 23, 19, 17 };
+    private readonly Aes _aes;
 
     /// <summary>
     /// Constructor
     /// </summary>
     /// <param name="strKey">Encryption key as string</param>
     public CronusAesProvider(string strKey) {
-        var _myAes = Aes.Create();
-        _myAes.KeySize = 256;
-        _myAes.Padding = PaddingMode.PKCS7;
-        _myAes.Key = CreateKey(strKey);
-        _myAes.IV = _vector;
-        _encryptor = _myAes.CreateEncryptor(_myAes.Key, _myAes.IV);
-        _decryptor = _myAes.CreateDecryptor(_myAes.Key, _myAes.IV);
+        _aes = Aes.Create();
+        _aes.KeySize = 256;
+        _aes.Padding = PaddingMode.PKCS7;
+        _aes.Key = CreateKey(strKey);
+        _aes.IV = _vector;
+        _aes.Mode = CipherMode.CBC;
     }
 
     // Creates a usable fixed length key from the string password
-    private static byte[] CreateKey(string strKey) {
-        return SHA256.HashData(Encoding.Unicode.GetBytes(strKey));
+    private static byte[] CreateKey(string strKey) => SHA256.HashData(Encoding.UTF8.GetBytes(strKey));
+
+    /// <summary>
+    /// Encrypts the text using the key
+    /// </summary>
+    /// <param name="unencrypted">original text</param>
+    /// <returns>Unicode string</returns>
+    public string Encrypt(string unencrypted) {
+        var buffer = Encoding.UTF8.GetBytes(unencrypted);
+        var result = _aes.EncryptCbc(buffer, _aes.IV);
+        return Convert.ToBase64String(result);
     }
 
-    public ICryptoTransform GetEncrypter() => _encryptor!;
-
-    public ICryptoTransform? GetDecrypter() => _decryptor!;
+    /// <summary>
+    /// Decrypts encrypted text
+    /// </summary>
+    /// <param name="encrypted">Encrypted text</param>
+    /// <returns>Transformed Unicode string</returns>
+    public string Decrypt(string encrypted) {
+        var buffer = Convert.FromBase64String(encrypted);
+        var result = _aes.DecryptCbc(buffer, _aes.IV);
+        return Encoding.UTF8.GetString(result);
+    }
 
     public void Dispose() {
-        _encryptor?.Dispose();
-        _decryptor?.Dispose();
+        _aes?.Dispose();
     }
 }
