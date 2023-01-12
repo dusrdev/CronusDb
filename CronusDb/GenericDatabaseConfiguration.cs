56907e6d80ff6bae3bbfdd5a36d6a539be77de63
David Shnayder <dusrdev@outlook.com>
2023-01-12 18:46:28 +0200

    Renamed type

--- CronusDb/GenericDatabaseConfiguration.cs
+++ CronusDb/GenericDatabaseConfiguration.cs
@@ -4,11 +4,16 @@
 /// Configuration for serializable databases
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public record SerializableDatabaseConfiguration<T> {
+public record GenericDatabaseConfiguration<T> {
     /// <summary>
     /// The path to the database file, including extension.
     /// </summary>
     public required string Path { get; init; }
+
+    /// <summary>
+    /// Automatically trigger serialization on Insert, Update and Delete.
+    /// </summary>
+    public required bool SerializeOnUpdate { get; init; }
 
     /// <summary>
     /// Encryption key for encrypting the database file with AES256.
