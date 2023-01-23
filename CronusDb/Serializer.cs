467c49ec545884470159c0a0b9849870039708a0
David Shnayder <dusrdev@outlook.com>
2023-01-23 21:12:46 +0200

    Extract all serialization logic to static helper, added converter too

---
+++ CronusDb/Serializer.cs
@@ -0,0 +1,29 @@
+﻿using MemoryPack;
+
+using System.Collections.Concurrent;
+
+namespace CronusDb;
+
+internal static class Serializer {
+    internal static void Serialize<TSerialized>(ConcurrentDictionary<string, TSerialized> data, string path, string? encryptionKey) {
+        var bin = MemoryPackSerializer.Serialize(data);
+        File.WriteAllBytes(path, string.IsNullOrWhiteSpace(encryptionKey) ? bin : bin.Encrypt(encryptionKey));
+    }
+
+    internal static async Task SerializeAsync<TSerialized>(ConcurrentDictionary<string, TSerialized> data, string path, string? encryptionKey) {
+        var bin = MemoryPackSerializer.Serialize(data);
+        await File.WriteAllBytesAsync(path, string.IsNullOrWhiteSpace(encryptionKey) ? bin : bin.Encrypt(encryptionKey));
+    }
+
+    internal static ConcurrentDictionary<string, TSerialized> Convert<TValue, TSerialized>(this ConcurrentDictionary<string, TValue> dict, Func<TValue, TSerialized> converter) {
+        var newDict = new ConcurrentDictionary<string, TSerialized>();
+        foreach (var (k, v) in dict) {
+            try {
+                newDict[k] = converter(v);
+            } catch {
+                throw new InvalidOperationException($"Converting the value of key <{k}> failed.");
+            }
+        }
+        return newDict;
+    }
+}
