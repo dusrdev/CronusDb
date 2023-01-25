1384edd384bfab37201707568a84427d92298eca
David Shnayder <dusrdev@outlook.com>
2023-01-25 20:04:06 +0200

    Added an enum to efficiently support multiple options for the database

---
+++ CronusDb/DatabaseOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace CronusDb;
+
+/// <summary>
+/// Database options
+/// </summary>
+public enum DatabaseOptions {
+    /// <summary>
+    /// Serialize database after upsert or delete.
+    /// </summary>
+    /// <remarks>
+    /// If <see cref="Database{TValue, TSerialzied}.RemoveAny(Func{TValue, bool})"/> is called serialization will only be done once, after execution is finished.
+    /// </remarks>
+    SerializeOnUpdate = 1 << 0,
+
+    /// <summary>
+    /// Trigger the OnDataChanged event on upsert and delete.
+    /// </summary>
+    /// <remarks>
+    /// Is disabled by default to improve performance
+    /// </remarks>
+    TriggerUpdateEvents = 1 << 1,
+}
