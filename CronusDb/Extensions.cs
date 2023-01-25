293e28979cb107e96f73e3c4ee28cd9935817373
David Shnayder <dusrdev@outlook.com>
2023-01-25 23:04:35 +0200

    Updated api's and added new public byte[] to and from string converters to improve flexibility while maximizing performance

--- CronusDb/Extensions.cs
+++ CronusDb/Extensions.cs
@@ -1,4 +1,6 @@
-﻿namespace CronusDb;
+﻿using System.Text;
+
+namespace CronusDb;
 
 internal static class Extensions {
     internal static byte[] Encrypt(this byte[] value, string key) {
@@ -6,18 +8,12 @@ internal static class Extensions {
         return aes.Encrypt(value);
     }
 
-    internal static string Encrypt(this string value, string key) {
-        using var aes = new CronusAesProvider(key);
-        return aes.Encrypt(value);
-    }
-
-    internal static byte[] Decrypt(this ReadOnlySpan<byte> value, string key) {
+    internal static byte[] Decrypt(this byte[] value, string key) {
         using var aes = new CronusAesProvider(key);
         return aes.Decrypt(value);
     }
 
-    internal static string Decrypt(this string value, string key) {
-        using var aes = new CronusAesProvider(key);
-        return aes.Decrypt(value);
-    }
+    public static byte[] ToByteArray(this string str) => Encoding.UTF8.GetBytes(str);
+
+    public static string ToUTF8String(this byte[] bytes) => Encoding.UTF8.GetString(bytes);
 }
