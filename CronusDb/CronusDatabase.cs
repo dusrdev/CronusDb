1809a35022fd6c1b3b744b815563d739f66d8226
David Shnayder <dusrdev@outlook.com>
2023-01-25 20:04:15 +0200

    Implemented new Options

--- CronusDb/CronusDatabase.cs
+++ CronusDb/CronusDatabase.cs
@@ -8,34 +8,18 @@ namespace CronusDb;
 public sealed class CronusDatabase : Database {
     private readonly ConcurrentDictionary<string, string> _data;
 
-    /// <summary>
-    /// Holds the configuration for this database.
-    /// </summary>
-    public DatabaseConfiguration Config { get; private init; }
+    /// <inheritdoc/>
+    public override DatabaseConfiguration Config { get; protected init; }
 
     internal CronusDatabase(ConcurrentDictionary<string, string>? data, DatabaseConfiguration config) {
         _data = data ?? new();
         Config = config;
     }
 
-    internal override void OnDataChanged(DataChangedEventArgs e) {
-        base.OnDataChanged(e);
-        if (Config.SerializeOnUpdate) {
-            Serialize();
-        }
-    }
-
-    /// <summary>
-    /// Checked whether the inner dictionary contains the <paramref name="key" />.
-    /// </summary>
-    /// <param name="key"></param>
+    /// <inheritdoc/>
     public override bool ContainsKey(string key) => _data.ContainsKey(key);
 
-    /// <summary>
-    /// Returns the value for the <paramref name="key" />.
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="encryptionKey">individual encryption key for this specific value</param>
+    /// <inheritdoc/>
     public override string? Get(string key, string? encryptionKey = null) {
         if (!_data.TryGetValue(key, out var val)) {
             return null;
@@ -46,46 +30,43 @@ public sealed class CronusDatabase : Database {
         return val.Decrypt(encryptionKey);
     }
 
-    /// <summary>
-    /// Removes the <paramref name="key" /> and its value from the inner dictionary.
-    /// </summary>
-    /// <param name="key"></param>
+    /// <inheritdoc/>
     public override bool Remove(string key) {
         if (!_data.TryRemove(key, out var val)) {
             return false;
         }
-        OnDataChanged(new DataChangedEventArgs {
-            Key = key,
-            Value = val,
-            ChangeType = DataChangeType.Remove
-        });
+        if (Config.Options.HasFlag(DatabaseOptions.SerializeOnUpdate)) {
+            Serialize();
+        }
+        if (Config.Options.HasFlag(DatabaseOptions.TriggerUpdateEvents)) {
+            OnDataChanged(new DataChangedEventArgs {
+                Key = key,
+                Value = val,
+                ChangeType = DataChangeType.Remove
+            });
+        }
         return true;
     }
 
-    /// <summary>
-    /// Saves the database to the hard disk.
-    /// </summary>
+    /// <inheritdoc/>
     public override void Serialize() => Serializer.Serialize(_data, Config!.Path, Config!.EncryptionKey);
 
-    /// <summary>
-    /// Saves the database to the hard disk.
-    /// </summary>
+    /// <inheritdoc/>
     public override Task SerializeAsync() => Serializer.SerializeAsync(_data, Config!.Path, Config!.EncryptionKey);
 
-    /// <summary>
-    /// Updates or inserts a new <paramref name="value" /> with the <paramref name="key" />.
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="value"></param>
-    /// <param name="encryptionKey">individual encryption key for this specific value</param>
-    /// <exception cref="ArgumentException"/>
+    /// <inheritdoc/>
     public override void Upsert(string key, [DisallowNull] string value, string? encryptionKey = null) {
         ArgumentException.ThrowIfNullOrEmpty(value);
         _data[key] = encryptionKey is null ? value : value.Encrypt(encryptionKey);
-        OnDataChanged(new DataChangedEventArgs {
-            Key = key,
-            Value = value,
-            ChangeType = DataChangeType.Upsert
-        });
+        if (Config.Options.HasFlag(DatabaseOptions.SerializeOnUpdate)) {
+            Serialize();
+        }
+        if (Config.Options.HasFlag(DatabaseOptions.TriggerUpdateEvents)) {
+            OnDataChanged(new DataChangedEventArgs {
+                Key = key,
+                Value = value,
+                ChangeType = DataChangeType.Upsert
+            });
+        }
     }
 }
