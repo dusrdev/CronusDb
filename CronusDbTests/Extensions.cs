11b2ab7eb01858b462ed1b04dd9c96f2f3770896
David Shnayder <dusrdev@outlook.com>
2023-05-15 16:52:26 +0300

    Copied extensions to prevent exposing internals

---
+++ CronusDbTests/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace CronusDbTests;
+
+internal static class Extensions {
+    public static byte[] ToByteArray(this string str) => Encoding.UTF8.GetBytes(str);
+
+    public static string ToUTF8String(this byte[] bytes) => Encoding.UTF8.GetString(bytes);
+}
